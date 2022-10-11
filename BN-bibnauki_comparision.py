b9cf00b605b24a4182092f1db3192042674a1bd1
darekpe79 <darekpe79@gmail.com>
2022-10-11 15:42:09 +0200

    changes changes

---
+++ BN-bibnauki_comparision.py
@@ -0,0 +1,122 @@
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from tqdm import tqdm
+import ijson
+import json
+import jellyfish
+with open ('C:/Users/User/Desktop/results.json', 'r', encoding='utf-8') as f:
+    nikodem=json.load(f)['records']
+files=["C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000001.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000002.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000003.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000004.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000005.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000006.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000007.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000008.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000009.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000010.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000011.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000012.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000013.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000014.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000015.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000016.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000017.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000018.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000019.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000020.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000021.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000022.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000023.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000024.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000025.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000026.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000027.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000028.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000029.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000030.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000031.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000032.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000033.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000034.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000035.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000036.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000037.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000038.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000039.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000040.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000041.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000042.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000043.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000044.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000045.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000046.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000047.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000048.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000049.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000050.json_results",
+"C:/Users/User/Desktop/BN_ARTICLES_JSON_HASHED-20221011T104350Z-001/BN_ARTICLES_JSON_HASHED/msplit00000051.json_results"]
+output={}
+for file in files:
+    with open (file, 'r', encoding='utf-8') as f2:
+        patryk=json.load(f2)['bn_records']
+    darek={'brak':[]} 
+       
+    for elem in patryk:
+        
+        if elem.get('773'):
+            for e in elem.get('773'):
+                if e.get('x'):
+                    issn=e.get('x').strip(' \'.,:;')
+                    if issn not in darek:
+                        darek[issn]=[(elem.get('001'), elem.get('245_hashed'),elem.get('650a')) ]
+                    else:
+                        darek[issn].append((elem.get('001'), elem.get('245_hashed'),elem.get('650a')))
+                else:
+                    
+                    darek['brak'].append((elem.get('001'), elem.get('245_hashed'),elem.get('650a')))
+                        
+           
+            
+       
+    for item in tqdm(nikodem):
+        titlebibNauk=item['article-title_hashed']
+        titlebibNauk_full=item['article-title']
+        identifier=item['identifier']
+        
+        issnbibNauk=item.get('issn')
+        if issnbibNauk in darek:
+
+            
+            for dp in darek[issnbibNauk]:
+                titleBibNar=dp[1]
+                idBibNar=dp[0]
+                haslo=dp[2]
+                if titlebibNauk==titleBibNar:
+                    if identifier not in output:
+                    
+                        output[identifier]=[(idBibNar,identifier,titleBibNar,titlebibNauk_full)]
+                    else:
+                        output[identifier].append((idBibNar,identifier,titleBibNar,titlebibNauk_full))
+                        
+                    
+
+        for dp in darek['brak']:
+            titleBibNar=dp[1]
+            idBibNar=dp[0]
+            haslo=dp[2]
+            if titlebibNauk==titleBibNar:
+                
+                    if identifier not in output:
+                    
+                        output[identifier]=[(idBibNar,identifier,titleBibNar,titlebibNauk_full)]
+                    else:
+                        output[identifier].append((idBibNar,identifier,titleBibNar,titlebibNauk_full))
+        
+with open ('json_all.json', 'w', encoding='utf-8') as file:
+    json.dump(output,file,ensure_ascii=False)           
+#jellyfish.levenshtein_distance(titlebibNauk, titleBibNar) <=1
