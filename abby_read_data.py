a7d31877628922d6659174ba0e35361e891fbd41
darekpe79 <darekpe79@gmail.com>
2024-02-29 09:38:57 +0100

    ABBY

---
+++ abby_read_data.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 09:29:24 2024
+
+@author: dariu
+"""
+
+import xml.etree.ElementTree as ET
+
+# Ścieżka do Twojego pliku XML
+file_path = 'D:/Nowa_praca/synopsisSJ_abbyy.xml'
+
+# Otwieranie i wczytywanie pliku XML
+tree = ET.parse(file_path)
+root = tree.getroot()
+
+# Definiujemy przestrzeń nazw, aby można było korzystać z niej podczas wyszukiwania
+namespaces = {'ns': 'http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml'}
+
+# Wyszukiwanie wszystkich bloków tekstowych
+text_blocks = root.findall('.//ns:block[@blockType="Text"]', namespaces)
+
+for block in text_blocks[:2]:
+    # Dla każdego bloku tekstowego wyszukujemy paragrafy
+    paragraphs = block.findall('.//ns:text/ns:par', namespaces)
+    for par in paragraphs:
+        # Dla każdego paragrafu wyszukujemy linie
+        lines = par.findall('.//ns:line', namespaces)
+        for line in lines:
+            # Dla każdej linii konstruujemy tekst z charParams
+            text_line = ''.join(char.text for char in line.findall('.//ns:charParams', namespaces))
+            print(text_line)
+            
+            
+            
+            
+block_counter = 1
+for block in text_blocks[:4]:  # Przetwarzamy tylko dwa pierwsze bloki
+    print(f"--- Blok Tekstowy {block_counter} ---")
+    # Dla każdego bloku tekstowego wyszukujemy paragrafy
+    paragraph_counter = 1
+    paragraphs = block.findall('.//ns:text/ns:par', namespaces)
+    for par in paragraphs:
+        print(f"  Paragraf {paragraph_counter}:")
+        # Dla każdego paragrafu wyszukujemy linie
+        line_counter = 1
+        lines = par.findall('.//ns:line', namespaces)
+        for line in lines:
+            # Konstruujemy tekst z charParams dla każdej linii
+            text_line = ''.join([char.text if char.text is not None else '' for char in line.findall('.//ns:charParams', namespaces)])
+            print(f"    Linia {line_counter}: {text_line}")
+            line_counter += 1
+        paragraph_counter += 1
+    block_counter += 1
