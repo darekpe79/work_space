3304b42ae5f2926e4624ba481881663de987fb87
darekpe79 <darekpe79@gmail.com>
2024-02-29 10:29:31 +0100

    abby

--- abby_read_data.py
+++ abby_read_data.py
@@ -35,7 +35,7 @@ for block in text_blocks[:2]:
 #lepsza wersja dla Czarka:         
             
 block_counter = 1
-for block in text_blocks[:4]:  # Przetwarzamy tylko dwa pierwsze bloki
+for block in text_blocks[:13]:  # Przetwarzamy tylko dwa pierwsze bloki
     print(f"--- Blok Tekstowy {block_counter} ---")
     # Dla każdego bloku tekstowego wyszukujemy paragrafy
     paragraph_counter = 1
@@ -52,3 +52,50 @@ for block in text_blocks[:4]:  # Przetwarzamy tylko dwa pierwsze bloki
             line_counter += 1
         paragraph_counter += 1
     block_counter += 1
+    
+    
+    
+    
+    
+page_counter = 1
+for page in root.findall('.//ns:page', namespaces)[1:2]:
+    print(f"--- Strona {page_counter} ---")
+    block_counter = 1
+    text_blocks = page.findall('.//ns:block[@blockType="Text"]', namespaces)
+    for block in text_blocks:
+        print(f"  --- Blok Tekstowy {block_counter} ---")
+        paragraph_counter = 1
+        paragraphs = block.findall('.//ns:text/ns:par', namespaces)
+        for par in paragraphs:
+            print(f"    Paragraf {paragraph_counter}:")
+            line_counter = 1
+            lines = par.findall('.//ns:line', namespaces)
+            for line in lines:
+                text_line = ''.join([char.text if char.text is not None else '' for char in line.findall('.//ns:charParams', namespaces)])
+                print(f"      Linia {line_counter}: {text_line}")
+                line_counter += 1
+            paragraph_counter += 1
+        block_counter += 1
+    page_counter += 1
+    
+    
+    
+page = root.find('.//ns:page', namespaces)  # Znajdź tylko pierwszą stronę
+if page is not None:
+    print("--- Strona 1 ---")
+    block_counter = 1
+    text_blocks = page.findall('.//ns:block[@blockType="Text"]', namespaces)
+    for block in text_blocks:
+        print(f"  --- Blok Tekstowy {block_counter} ---")
+        paragraph_counter = 1
+        paragraphs = block.findall('.//ns:text/ns:par', namespaces)
+        for par in paragraphs:
+            print(f"    Paragraf {paragraph_counter}:")
+            line_counter = 1
+            lines = par.findall('.//ns:line', namespaces)
+            for line in lines:
+                text_line = ''.join([char.text if char.text is not None else '' for char in line.findall('.//ns:charParams', namespaces)])
+                print(f"      Linia {line_counter}: {text_line}")
+                line_counter += 1
+            paragraph_counter += 1
+        block_counter += 1
